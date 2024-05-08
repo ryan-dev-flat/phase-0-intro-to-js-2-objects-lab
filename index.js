74222343278e09e1e0884c3744cff8d3576ae205
Ryan <coap.ryanmontgomery@gmail.com>
2024-05-08 15:10:59 -0600

    this creates myFile.md

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// Define the 'employee' object
+let employee = {
+    name: "Employee Name",
+    streetAddress: "123 Main St"
+};
+
+// Define the 'updateEmployeeWithKeyAndValue()' function
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+
+// Define the 'destructivelyUpdateEmployeeWithKeyAndValue()' function
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// Define the 'deleteFromEmployeeByKey()' function
+function deleteFromEmployeeByKey(employee, key) {
+    let newObj = { ...employee };
+    delete newObj[key];
+    return newObj;
+}
+
+// Define the 'destructivelyDeleteFromEmployeeByKey()' function
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
